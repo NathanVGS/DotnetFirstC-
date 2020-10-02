a7adff2bf43b8f613a4080727f0bad972ff037c9
NathanVGS <vangulcknathan@gmail.com>
2020-10-02 12:25:09 +0200

    .net console + writing to files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create file with name: ");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Write content to the file: ");
+            string content = Console.ReadLine();
+            File.WriteAllText("files/" + fileName, content);
+            string input = Console.ReadLine();
+            
+            string outputFile = File.ReadAllText(input);
+            Console.WriteLine(outputFile);
+        } 
+    }
+}
